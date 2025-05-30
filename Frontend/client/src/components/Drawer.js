5e094d336f426bb2f2860b387310784e22f9a688
Sai Krishna <saikrishnajv22@gmail.com>
2025-05-30 16:17:29 +0530

    created a new file and moved components to that file

--- Frontend/client/src/components/Drawer.js
+++ Frontend/client/src/components/Drawer.js
@@ -1,40 +1,33 @@
-import { Container, Typography } from '@mui/material'
-import Drawer from './Drawer'
-// import { Box, Container, IconButton, InputAdornment, List, OutlinedInput, Skeleton, Typography } from '@mui/material'
-// import React, { useState } from 'react'
-// import SearchIcon from '@mui/icons-material/Search';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { fetchUser } from '../Slices/ChatSlice';
-// import UsersData from './UsersData.js';
-// import Cookies from "js-cookie"
+import { Box, IconButton, InputAdornment, List, OutlinedInput, Skeleton } from '@mui/material'
+import React, { useState } from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUser } from '../Slices/ChatSlice';
+import UsersData from './UsersData.js';
+import Cookies from "js-cookie"
 
-const SideDrawer = ({ toggleDrawer }) => {
-    // const [search, setSearch] = useState("")
-    // const { loading, newUsers } = useSelector(state => state.chat)
+const Drawer = ({toggleDrawer,onClickUser}) => {
+    const [search, setSearch] = useState("")
+    const { loading, newUsers } = useSelector(state => state.chat)
 
-    // const dispatch = useDispatch()
-    // const jwt = Cookies.get("jwToken")
+    const dispatch = useDispatch()
+    const jwt = Cookies.get("jwToken")
 
 
 
 
-    // const handleSearch = (e) => {
-    //     if (e.key === "Enter") {
-    //         dispatch(fetchUser({jwt,search}))
-    //     }
-    // }
-    // const handleIcon = () => {
-    //     dispatch(fetchUser({jwt, search}))
+    const handleSearch = (e) => {
+        if (e.key === "Enter") {
+            dispatch(fetchUser({ jwt, search }))
+        }
+    }
+    const handleIcon = () => {
+        dispatch(fetchUser({ jwt, search }))
 
-    // }
+    }
     return (
         <>
-            <Container sx={{ width: { xs: "60vw", sm: "47vw", md: "35vw", lg: "30vw" }, marginTop: '20px', display: 'flex', flexDirection: 'column', flexWrap: 'wrap' }} >
-                <Typography sx={{ fontSize: { xs: "20px", md: "24px" } }} marginBottom="20px">
-                    Search New Users
-                </Typography>
-                <Drawer toggleDrawer={toggleDrawer}/>
-                {/* <Box component='div' sx={{
+        <Box component='div' sx={{
                     display: 'flex',
                     alignItems: 'center',
                     gap: '10px'
@@ -74,14 +67,13 @@ const SideDrawer = ({ toggleDrawer }) => {
 
                             {newUsers.map(user => {
                                 //  return <p>{JSON.stringify(user)}</p>
-                                return <UsersData toggleDrawer={toggleDrawer} user={user} key={user._id} />
+                                return <UsersData toggleDrawer={toggleDrawer} onClickUser={onClickUser} user={user} key={user._id} />
                             })}
                         </List>
-                    </>} */}
+                    </>}
 
-            </Container>
         </>
     )
 }
 
-export default SideDrawer
+export default Drawer
