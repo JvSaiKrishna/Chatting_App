839c16146c0f38f082cb64349039a3bfbd0dcaa2
Sai Krishna <saikrishnajv22@gmail.com>
2025-05-30 15:33:05 +0530

    Added new routers

--- Backend/router/chatRouter.js
+++ Backend/router/chatRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { AuthMiddleware } from "../middleware/authMiddleware.js";
-import { createChat ,fetchChats, groupChat, renameGroup,addNewMember,removeMember } from "../controllers/chatController.js";
+import { createChat ,fetchChats, groupChat, renameGroup,addNewMember,removeMember, groupPicUpdate } from "../controllers/chatController.js";
 import { fetchMessages, sendMessage } from "../controllers/messageController.js";
 
 const router = express.Router()
@@ -9,9 +9,9 @@ router.post('/',AuthMiddleware,createChat)
 router.get('/',AuthMiddleware,fetchChats)
 router.post('/group',AuthMiddleware,groupChat)
 router.put('/rename',AuthMiddleware,renameGroup)
-router.post('/group/addMember',AuthMiddleware,addNewMember)
-router.post('/group/removeMember',AuthMiddleware,removeMember)
-
+router.put('/group/addMember',AuthMiddleware,addNewMember)
+router.put('/group/removeMember',AuthMiddleware,removeMember)
+router.put("/group/:id",AuthMiddleware,groupPicUpdate)
 
 router.post('/message',AuthMiddleware,sendMessage)
 router.get('/:id/message',AuthMiddleware,fetchMessages)
