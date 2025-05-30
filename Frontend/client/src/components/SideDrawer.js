565ad89fcb464a5eca46f2fb39263b178116cd99
Sai Krishna <saikrishnajv22@gmail.com>
2025-05-30 16:19:30 +0530

    Removed unwanted comments

--- Frontend/client/src/components/SideDrawer.js
+++ Frontend/client/src/components/SideDrawer.js
@@ -1,32 +1,11 @@
 import { Container, Typography } from '@mui/material'
 import Drawer from './Drawer'
-// import { Box, Container, IconButton, InputAdornment, List, OutlinedInput, Skeleton, Typography } from '@mui/material'
-// import React, { useState } from 'react'
-// import SearchIcon from '@mui/icons-material/Search';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { fetchUser } from '../Slices/ChatSlice';
-// import UsersData from './UsersData.js';
-// import Cookies from "js-cookie"
 
 const SideDrawer = ({ toggleDrawer }) => {
-    // const [search, setSearch] = useState("")
-    // const { loading, newUsers } = useSelector(state => state.chat)
-
-    // const dispatch = useDispatch()
-    // const jwt = Cookies.get("jwToken")
 
 
 
 
-    // const handleSearch = (e) => {
-    //     if (e.key === "Enter") {
-    //         dispatch(fetchUser({jwt,search}))
-    //     }
-    // }
-    // const handleIcon = () => {
-    //     dispatch(fetchUser({jwt, search}))
-
-    // }
     return (
         <>
             <Container sx={{ width: { xs: "60vw", sm: "47vw", md: "35vw", lg: "30vw" }, marginTop: '20px', display: 'flex', flexDirection: 'column', flexWrap: 'wrap' }} >
@@ -34,51 +13,7 @@ const SideDrawer = ({ toggleDrawer }) => {
                     Search New Users
                 </Typography>
                 <Drawer toggleDrawer={toggleDrawer}/>
-                {/* <Box component='div' sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    gap: '10px'
-
-                }}>
-                    <OutlinedInput
-                        placeholder='Search ..'
-                        fullWidth
-                        onChange={(e) =>
-                            setSearch(e.target.value)}
-                        onKeyDown={handleSearch}
-                        endAdornment={
-
-                            <InputAdornment position='end'>
-                                <IconButton onClick={handleIcon} edge="end">
-                                    <SearchIcon fontSize='large' sx={{ display: { xs: 'none', sm: "inline" } }} />
-
-                                </IconButton>
-                            </InputAdornment>
-                        }
-
-                    />
-                </Box>
-                {loading ?
-                    <>
-                        <Skeleton animation="wave" sx={{ height: "70px" }} />
-                        <Skeleton animation="wave" sx={{ height: "70px" }} />
-                        <Skeleton animation="wave" sx={{ height: "70px" }} />
-                        <Skeleton animation="wave" sx={{ height: "70px" }} />
-                        <Skeleton animation="wave" sx={{ height: "70px" }} />
-                        <Skeleton animation="wave" sx={{ height: "70px" }} />
-                        <Skeleton animation="wave" sx={{ height: "70px" }} />
-                    </>
-                    :
-                    <>
-                        <List>
-
-                            {newUsers.map(user => {
-                                //  return <p>{JSON.stringify(user)}</p>
-                                return <UsersData toggleDrawer={toggleDrawer} user={user} key={user._id} />
-                            })}
-                        </List>
-                    </>} */}
-
+               
             </Container>
         </>
     )
