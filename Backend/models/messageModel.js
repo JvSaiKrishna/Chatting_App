57c6ce587e4c9bbb063e29326b245da26665cf86
Sai Krishna <saikrishnajv22@gmail.com>
2025-05-30 15:17:14 +0530

    Updated message schema

--- Backend/models/messageModel.js
+++ Backend/models/messageModel.js
@@ -13,7 +13,8 @@ const messageSchma = new mongoose.Schema({
     chat:{type:mongoose.Schema.Types.ObjectId,
         ref:"chat",
         
-    }
+    },
+    left:{type:Boolean,required:true}
 
 },{ timestamps: true })
 
